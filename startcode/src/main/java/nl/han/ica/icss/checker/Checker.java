93a6b954a36fd3cfe5553e35874d6ca375153ec5
Freek856 <f.vandreumel@student.han.nl>
2024-03-24 15:58:45 +0100

    selector check

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -1,6 +1,9 @@
 package nl.han.ica.icss.checker;
 
 import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.selectors.ClassSelector;
+import nl.han.ica.icss.ast.selectors.IdSelector;
+import nl.han.ica.icss.ast.selectors.TagSelector;
 import nl.han.ica.icss.ast.types.ExpressionType;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -12,13 +15,14 @@ public class Checker {
     public void check(AST ast) {
         variableTypes = new LinkedList<>();
         System.out.println("CHECKEN...");
+        checkStylesheet(ast.root);
     }
 
     private void checkStylesheet(Stylesheet stylesheet){
         variableTypes.addFirst(new HashMap<>());
         for(ASTNode ast : stylesheet.getChildren()){
             if(ast instanceof Stylerule){
-                //checkstylerule
+                checkStyleRule((Stylerule) ast);
             }
             else if (ast instanceof VariableAssignment){
                 //checkvariableassignment
@@ -28,5 +32,30 @@ public class Checker {
             }
         }
         variableTypes.removeFirst();
+    }
+
+    private void checkStyleRule(Stylerule stylerule) {
+        System.out.println("Stylerule: " + stylerule.body.toString());
+        for (Selector selector : stylerule.selectors){
+            checkSelector(selector);
+        }
+        for (ASTNode ast : stylerule.body){
+            //checkinline
+        }
+    }
+
+    private void checkSelector(Selector selector){
+        if(selector instanceof IdSelector){
+            System.out.println("IdSelector: " + selector);
+        }
+        else if (selector instanceof ClassSelector){
+            System.out.println("idSelector: " + selector);
+        }
+        else if (selector instanceof TagSelector){
+            System.out.println("TagSelector: " + selector);
+        }
+        else {
+            System.out.println("Geen selector gevonden");
+        }
     }
 }
