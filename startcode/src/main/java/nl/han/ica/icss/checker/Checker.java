7f4f096e64bb031d80eba2f4a7377b6c890173d9
Freek856 <f.vandreumel@student.han.nl>
2024-03-24 15:43:44 +0100

    Begin gemaakt aan de checker

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -1,21 +1,32 @@
 package nl.han.ica.icss.checker;
 
-import nl.han.ica.datastructures.IHANLinkedList;
-import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.types.ExpressionType;
-
 import java.util.HashMap;
-
+import java.util.LinkedList;
 
 
 public class Checker {
-
-    private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
+    private LinkedList<HashMap<String, ExpressionType>> variableTypes;
 
     public void check(AST ast) {
-        // variableTypes = new HANLinkedList<>();
-
+        variableTypes = new LinkedList<>();
+        System.out.println("CHECKEN...");
     }
 
-    
+    private void checkStylesheet(Stylesheet stylesheet){
+        variableTypes.addFirst(new HashMap<>());
+        for(ASTNode ast : stylesheet.getChildren()){
+            if(ast instanceof Stylerule){
+                //checkstylerule
+            }
+            else if (ast instanceof VariableAssignment){
+                //checkvariableassignment
+            }
+            else {
+                ast.setError("Geen rule of variabel gevonden!");
+            }
+        }
+        variableTypes.removeFirst();
+    }
 }
