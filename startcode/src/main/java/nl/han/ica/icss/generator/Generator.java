8cd8e0db663bed41aa8ebe8abe3e5262c4bdc6f5
Freek856 <f.vandreumel@student.han.nl>
2024-03-28 10:08:57 +0100

    Generator werkende

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -1,10 +1,11 @@
 package nl.han.ica.icss.generator;
 
 
-import nl.han.ica.icss.ast.AST;
-import nl.han.ica.icss.ast.ASTNode;
-import nl.han.ica.icss.ast.Stylerule;
-import nl.han.ica.icss.ast.Stylesheet;
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.ColorLiteral;
+import nl.han.ica.icss.ast.literals.PercentageLiteral;
+import nl.han.ica.icss.ast.literals.PixelLiteral;
+import nl.han.ica.icss.ast.literals.ScalarLiteral;
 
 public class Generator {
 
@@ -30,10 +31,36 @@ public class Generator {
 		StringBuilder StyleRule = new StringBuilder();
 		StyleRule.append(stylerule.selectors.get(0).toString()).append(" {\n");
 		for (int x = 0; x < stylerule.body.size(); x++){
-			StyleRule.append(" ").append(stylerule.body.get(x).toString()).append("\n");
+			StyleRule.append(generateDeclaration((Declaration) stylerule.body.get(x)));
 		}
 		StyleRule.append("}\n");
 		return StyleRule.toString();
 	}
 
+	//Genereert de Declarations met al de property, de literals erachter moeten nog uit de expressies gehaald worden in de volgende functie
+	private String generateDeclaration(Declaration declaration) {
+		StringBuilder Declaration = new StringBuilder();
+		for (int y = 0; y < declaration.getChildren().size(); y = y + 2){
+			Declaration.append("").append(declaration.property.name.toString()).append(": ");
+			Declaration.append(generateLiteral((Literal) declaration.expression)).append(";\n");
+		}
+		return Declaration.toString();
+	}
+
+	//Hier wordt de juiste Literal variant geretourneerd
+	private String generateLiteral(Literal literal) {
+		if (literal instanceof ColorLiteral){
+			return ((ColorLiteral) literal).value;
+		}
+		else if (literal instanceof PixelLiteral){
+			return ((PixelLiteral) literal).value + "px";
+		}
+		else if (literal instanceof PercentageLiteral){
+			return ((PercentageLiteral) literal).value + "%";
+		}
+		else if (literal instanceof ScalarLiteral){
+			return ((ScalarLiteral) literal).value + "";
+		}
+		else return null;
+	}
 }
